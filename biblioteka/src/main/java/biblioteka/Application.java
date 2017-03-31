832643bd7e6c33933c7e59c72612dcc2ae83d0c7
DJ <djkos22@onet.pl>
2017-03-31 16:14:35 +0200

    uruchomienie strony startowej

---
+++ biblioteka/src/main/java/biblioteka/Application.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package biblioteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Damian
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    /**Running application
+     * @param args arguments from command line
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ biblioteka/src/main/java/biblioteka/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package biblioteka.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	String index(){
+		return "index";
+	}
+}
+ 
