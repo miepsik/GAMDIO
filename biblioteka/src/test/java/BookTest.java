6468b582b78971f8c21e26758cade95fc9017c6a
Grzegorz Miebs <grzegorz.miebs@o2.pl>
2017-05-06 09:46:16 +0200

    More Book tests

--- biblioteka/src/test/java/BookTest.java
+++ biblioteka/src/test/java/BookTest.java
@@ -1,8 +1,11 @@
+import biblioteka.model.Author;
 import biblioteka.model.Book;
 import biblioteka.model.Position;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashSet;
 
 import static org.mockito.Mockito.*;
 import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.After;
@@ -13,12 +16,13 @@ public class BookTest {
 
     public Book book;
     Position mock;
+    Author author;
 
     @Before
     public void setUp() throws Exception {
         book = new Book();
         mock = mock(Position.class);
-
+        author = mock(Author.class);
     }
 
     @After
@@ -30,16 +34,57 @@ public class BookTest {
         when(mock.getBook()).thenReturn(book);
         book.addPosition(mock);
         assertEquals((int) book.getAmount(), 1);
+        verify(mock, times(0)).setBook(isA(Book.class));
+    }
+
+    @Test
+    public void testAddPosition1() throws Exception {
+        when(mock.getBook()).thenReturn(new Book());
+        book.addPosition(mock);
+        assertEquals((int) book.getAmount(), 1);
+        verify(mock, times(1)).setBook(isA(Book.class));
     }
 
     @Test
     public void testRemovePosition() throws Exception {
         when(mock.getBook()).thenReturn(book);
         book.addPosition(mock);
+        verify(mock, times(0)).setBook(isA(Book.class));
+        when(mock.getBook()).thenReturn(new Book());
+        book.removePosition(mock);
+        verify(mock, times(0)).setBook(isA(Book.class));
+        assertEquals((int) book.getAmount(), 0);
+    }
+
+    @Test
+    public void testRemovePosition1() throws Exception {
+        when(mock.getBook()).thenReturn(new Book());
+        book.addPosition(mock);
+        verify(mock, times(1)).setBook(isA(Book.class));
         when(mock.getBook()).thenReturn(new Book());
         book.removePosition(mock);
         assertEquals((int) book.getAmount(), 0);
+        verify(mock, times(1)).setBook(isA(Book.class));
     }
+
+    @Test
+    public void testSetAuthor() throws Exception {
+        HashSet<Book> hs = new HashSet<>();
+        hs.add(book);
+        when(author.getBooks()).thenReturn(hs);
+        book.setAuthor(author);
+        verify(author, times(0)).addBook(isA(Book.class));
+    }
+
+    @Test
+    public void testSetAuthor1() throws Exception {
+        HashSet<Book> hs = new HashSet<>();
+        hs.add(new Book());
+        when(author.getBooks()).thenReturn(hs);
+        book.setAuthor(author);
+        verify(author, times(1)).addBook(isA(Book.class));
+    }
+
 
     @Test
     public void testToString() throws Exception {
